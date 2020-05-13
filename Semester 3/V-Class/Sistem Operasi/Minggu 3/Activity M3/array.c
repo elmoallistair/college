55c924c1df3224167477de60fe41b57a41d92905
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-05-13 12:36:06 +0700

    Create array.c

---
+++ Semester 3/V-Class/Sistem Operasi/Minggu 3/Activity M3/array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num[100], n;
+    int sum = 0;
+    printf("Enter number of elements: "); 
+    scanf("%d", &n);
+    printf("\nInput numbers :\n");
+    for(int i=0; i<n; ++i) {
+        printf("Number %d: ", i+1);
+        scanf("%d", &num[i]);
+        sum += num[i];
+    }
+    printf("\nAverage = %d", sum/n);
+    
+    return 0;
+}
