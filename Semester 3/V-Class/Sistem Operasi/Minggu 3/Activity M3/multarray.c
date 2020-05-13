e34840ce2b7591c90c6c815d91dd8d32907bcff8
Elmo Allistair <55618192+elmoallistair@users.noreply.github.com>
2020-05-13 12:42:13 +0700

    Create multarray.c

---
+++ Semester 3/V-Class/Sistem Operasi/Minggu 3/Activity M3/multarray.c
@@ -0,0 +1,34 @@
+// Sum of 2x2 Matrices
+
+#include <stdio.h>
+int main() {
+    float a[2][2], b[2][2], result[2][2];
+    
+    printf("Enter elements of 1st matrix\n");
+    for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j) {
+        printf("A[%d][%d]: ", i+1, j+1);
+        scanf("%f", &a[i][j]);
+    }
+
+    printf("Enter elements of 2nd matrix\n");
+    for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j) {
+        printf("B[%d][%d]: ", i+1, j+1);
+        scanf("%f", &b[i][j]);
+    }
+
+    for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+    result[i][j] = a[i][j] + b[i][j];
+
+    printf("\nSum Of Matrix: \n");
+    for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j) {
+        printf("%.1f\t", result[i][j]);
+        if (j == 1)
+        printf("\n");
+    }
+    
+    return 0;
+}
